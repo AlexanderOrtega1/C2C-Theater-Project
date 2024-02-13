d2e98f6b583b49fa88089a635db65d06110975bf
AlexanderOrtega1 <154935339+AlexanderOrtega1@users.noreply.github.com>
2024-02-13 23:41:52 +0000

    Added display of all available seats

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import theaterSeats as t
 
 
-for row in t.theater_seats:  # goes through the set of lists
-  for seat in row:           # defines each seat in each list
-    print(seat, end='\t')    # prints each seat out in
-  print('\n')
+#for row in t.theater_seats:  # goes through the set of lists
+  #for seat in row:           # defines each seat in each list
+    #print(seat, end='\t')    # prints each seat out in
+  #print('\n')
   
 print()
 
@@ -14,3 +14,17 @@ for row in t.theater_seats2:
     print(seat, end='\t')
   print('\n')
 
+empty_seats = []
+
+for row in t.theater_seats2:
+  index = 0
+  for seat in row:
+    if seat == 'o':
+      empty_seat = f'{row[0]}{index}'
+      empty_seats.append(empty_seat)
+    else:
+      pass
+    index += 1
+
+for i in empty_seats:
+  print(i)
--- theaterSeats.py
+++ theaterSeats.py
@@ -1,6 +1,18 @@
 #Shows the structure of the theater map with all seats empty
-theater_seats = [[0,1,2,3,4,5,6],['A','o','o','o','o','o','o'],['B','o','o','o','o','o','o'], ['C','o','o','o','o','o','o'], ['D', 'o','o','o','o','o','o'], ['E', 'o','o','o','o','o','o'], ['F','o','o','o','o','o','o']]
+theater_seats = [[0,1,2,3,4,5,6],
+                 ['A','o','o','o','o','o','o'],
+                 ['B','o','o','o','o','o','o'],
+                 ['C','o','o','o','o','o','o'],
+                 ['D','o','o','o','o','o','o'],
+                 ['E','o','o','o','o','o','o'],
+                 ['F','o','o','o','o','o','o']]
 
 #This will serve as a test case, where 'x' represents a seat that is take and 'o' represnts an open seat
-theater_seats2 = [[0, 1, 2, 3, 4, 5, 6],['A','x','x','x','o','o','x'],['B','x','o','x','o','x','o'], ['C','x','x','x','x','x','x'], ['D', 'x','o','o','x','x','x'], ['E', 'x','x','o','x','x','o'], ['F','o','o','o','o','o','x']]
+theater_seats2 = [['0','1','2','3','4','5','6'],
+                  ['A','x','x','x','o','o','x'],
+                  ['B','x','o','x','o','x','o'],
+                  ['C','x','x','x','x','x','x'],
+                  ['D','x','o','o','x','x','x'],
+                  ['E','x','x','o','x','x','o'],
+                  ['F','o','o','o','o','o','x']]
 
